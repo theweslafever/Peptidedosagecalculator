abe8fab3d8a6c6ce971c9d6d41c8b8138794e223
Life Peptides <the.wes.lafever@gmail.com>
2026-06-01 19:28:41 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -17,6 +16,8 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  // Tells GitHub Pages to look inside the /Pdc/ folder structure
+  base: '/Pdc/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
@@ -30,7 +31,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
-})
